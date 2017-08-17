c835e151c77c0377930d8375bd3a5f4a2d7cde66
b404139 <b404139@users.noreply.github.com>
2017-08-17 18:10:35 +0800

    maven-test-base-jar init

---
+++ src/main/java/cn/bigg/base/Base.java
@@ -0,0 +1,16 @@
+package cn.bigg.base;
+
+/**
+ * 
+ * @author ${bigg}
+ *
+ */
+public class Base {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Maven");
+	}
+}
